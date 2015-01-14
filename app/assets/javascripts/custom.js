00bb4fe43b41ff63f0cb86ce72f4599887c00b71
Mark Natera <marknatera@gmail.com>
2015-01-14 00:25:43 -0500

    Navbar working

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,29 @@
-$(window).on('scroll', function(){
-  var logo = $('.nav-apply');
-  var w = $(window);
-  if(w.scrollTop() > 350){
-    logo.stop(true, false).fadeTo(100, 1.0);
-  } else if(w.scrollTop() < 350) {
-    logo.stop(true, false).fadeTo(100, 0);
-  }
-});
+// $(window).on('scroll', function(){
+//   var logo = $('.header-16');
+//   var w = $(window);
+//   if(w.scrollTop() > 350){
+//     logo.stop(true, false).fadeTo(100, 1.0);
+//   } else if(w.scrollTop() < 350) {
+//     logo.stop(true, false).fadeTo(100, 0);
+//   }
+// });
 
 $(window).scroll(function() {
     $("#logo-main").css({
     'opacity' : 1-(($(this).scrollTop())/300)
     });          
+});
+
+$(document).ready(function() {
+  $(window).scroll(function(){
+    if(document.body.scrollTop > 300)
+      $('#header').fadeIn( "slow", function() { });
+    else  
+      $('#header').fadeOut( "slow", function() { });
+  });
+
+  $('a#srolltotop').click(function(){
+    $('html, body').animate({scrollTop:0}, 100);
+    return false;
+  });
 });
