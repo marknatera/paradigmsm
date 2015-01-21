fd6dbafc7f20c6fa3080697afeb93bc4d4d3296d
Mark Natera <mark@truthkingdom.com>
2015-01-20 22:00:12 -0500

    Trying out some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'def/send_mail'
+
   root to: 'pages#home'
   get '/about' => 'pages#about'
   get '/services' => 'pages#services'
@@ -10,6 +12,9 @@ Rails.application.routes.draw do
   get '/clients' => 'pages#clients'
   get '/contact' => 'pages#contact'
   get '/media' => 'pages#media'
+
+  resources :contact
+  match '/send_mail', to: 'contact#send_mail', via: 'post'
 
   devise_for :users
   resources :users
